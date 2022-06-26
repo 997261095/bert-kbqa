490ad0a62e4bf9490aaec1fabdba81a631b55984
henry_23 <henry.maoht@outlook.com>
2022-06-26 12:42:53 +0800

    fix: improve code for 'ConstructTriple'

--- input/data/ConstructTriple.py
+++ input/data/ConstructTriple.py
@@ -1,56 +1,55 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2019/4/18 20:16
-# @Author  : Alan
-# @Email   : xiezhengwen2013@163.com
-# @File    : triple_clean.py
-# @Software: PyCharm
 
-
-import pandas as pd
-
+import os
+import csv
+from Configurations import DATA_DIR, DB_DATA_DIR
+from MyUtils import line_parse, my_strip
 
 '''
-构造NER训练集，实体序列标注，用于训练BERT+BiLSTM+CRF
+构造 NER 训练集 (即三元组), 实体序列标注, 用于训练 BERT + BiLSTM + CRF
 '''
 
-question_str = "<question"
-triple_str = "<triple"
-answer_str = "<answer"
-start_str = "============="
+# 将处理后的文件写在 ./input/DB_data 下
+if not os.path.exists(DB_DATA_DIR):
+    os.mkdir(DB_DATA_DIR)
 
+filename_prefix = 'nlpcc-iccpol-2016.kbqa.'
+filename_suffix = '-data'
+file_name_list = [
+    filename_prefix + var + filename_suffix for var in ['training', 'testing']
+]
 
-triple_list = []
-seq_q_list = []    #["中","华","人","民"]
-seq_tag_list = []  #[0,0,1,1]
+with open(os.path.join(DB_DATA_DIR, 'clean_triple.csv'), mode='w', encoding='utf-8', newline='') as out_csv_file:
+    # 三元组: 实体, 属性, 答案
+    writer = csv.DictWriter(out_csv_file, fieldnames=['entity', 'attribute', 'answer'])
+    writer.writeheader()
 
-for data_type in ["training", "testing"]:
-    file = "./NLPCC2016KBQA/nlpcc-iccpol-2016.kbqa." + data_type + "-data"
-    with open(file, 'r',encoding='utf-8') as f:
-        q_str = ""  # question
-        t_str = ""  # triple
-        a_str = ""  # answer
-        for line in f:
-            if question_str in line:
-                q_str = line.strip()
-            if triple_str in line:
-                t_str = line.strip()
-
-            # 一个新的三元组
-            if start_str in line:
-                entities = t_str.split("|||")[0].split(">")[1].strip()
-                q_str = q_str.split(">")[1].replace(" ","").strip()
-                if ''.join(entities.split(' ')) in q_str:
-                    clean_triple = t_str.split(">")[1].replace('\t','').replace(" ","").strip().split("|||")
-                    triple_list.append(clean_triple)
-                else:
-                    print(entities)
-                    print(q_str)
-                    print('------------------------')
-
-# 三元组：实体，属性，答案
-df = pd.DataFrame(triple_list, columns=["entity", "attribute", "answer"])
-print(df)
-print(df.info())
-
-# 处理完后将文件写在 ./input/DB_Data 下
-df.to_csv("./DB_Data/clean_triple.csv", encoding='utf-8', index=False)
+    for filename in file_name_list:
+        file_path = os.path.join(DATA_DIR, filename)
+        assert os.path.exists(file_path)
+        
+        with open(file_path, mode='r', encoding='utf-8') as f:
+            while True:
+                try:
+                    # 一次读取三行
+                    l = [line_parse(f.__next__()) for i in range(3)]
+                    # 并映射到字典的形式
+                    s = {t[0]: t[1] for t in l}
+                    # 跳过分割线行
+                    f.__next__()
+                    
+                    triples = [t.strip() for t in s['triple'].split('|||')]
+                    t = {
+                        'entity': triples[0],
+                        'attribute': triples[1],
+                        'answer': triples[2],
+                    }
+                    if t['entity'] in s['question']:
+                        writer.writerow(t)
+                    else:
+                        # print(s['question'])
+                        # print(t['entity'])
+                        # print('-' * 30)
+                        pass
+                except StopIteration:
+                    break
